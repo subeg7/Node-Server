341906bb6d9dd41f255c82d57143451a11f2f70b
Subeg Aryal <subeg7@gmail.com>
2020-04-16 10:23:48 +0545

    dev : minimal routes added

--- NepalTravel.js
+++ NepalTravel.js
@@ -4,12 +4,21 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/', function (req, res) {
+    res.type('text/plain');
+    res.send('Nepal Travel');
+});
+
+app.get('/about', function (req, res) {
+    res.type('text/plain');
+    res.send('See All About This travel itinerary');
+});
 
 //custom 404 page
 app.use(function (req, res) {
     res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.send('404 - The route you entered is not found');
 });
 
 //custom 500 page
