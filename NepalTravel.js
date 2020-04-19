3210d3dfbe4d4c860b1a1594dd7acf18015de293
Subeg Aryal <subeg7@gmail.com>
2020-04-19 09:54:32 +0545

    dev : rendering views with handlebars

--- NepalTravel.js
+++ NepalTravel.js
@@ -2,31 +2,33 @@ var express = require('express');
 
 var app = express();
 
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+    .create({ defaultLayout: 'main' }); //uses layouts.main as the default layout
+app.engine('handlebars', handlebars.engine); 
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Nepal Travel');
+   res.render('home'); //home view is put on top of the default layout
 });
 
 app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('See All About This travel itinerary');
+    res.render('about');
 });
 
 //custom 404 page
 app.use(function (req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - The route you entered is not found');
+    res.render('404');
 });
 
 //custom 500 page
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function () {
