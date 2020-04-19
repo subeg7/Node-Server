b3674faffb2fbe383ea37778ce2d8a0d822196ed
Subeg Aryal <subeg7@gmail.com>
2020-04-19 10:01:44 +0545

    dev : rendering dynamic data

--- NepalTravel.js
+++ NepalTravel.js
@@ -5,17 +5,29 @@ var app = express();
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
     .create({ defaultLayout: 'main' }); //uses layouts.main as the default layout
-app.engine('handlebars', handlebars.engine); 
+app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-   res.render('home'); //home view is put on top of the default layout
+    var travelPackages = [
+        "Pokhara",
+        "Sauraha",
+        "Dadeldhura"
+    ];
+    res.render('home'); //home view is put on top of the default layout
 });
 
 app.get('/about', function (req, res) {
-    res.render('about');
+    var ourAwards = [
+        "Best TA",
+        "Best tourer",
+        "Clean Visits"
+    ];
+    res.render('about',{ourAwards : ourAwards});
 });
 
 //custom 404 page
